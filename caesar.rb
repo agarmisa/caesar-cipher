0ef5c4fd9c19457ef35fa017eb2cd69089380bbb
agarmisa <alisongarmisa@gmail.com>
2015-10-31 13:16:30 -0400

    could be refactored more

--- caesar.rb
+++ caesar.rb
@@ -1,30 +1,29 @@
 
 class Caesar
-  attr_reader :alph
   def initialize(shift, alph = "abcdefghijklmnopqrstuvwxyz" )
     @shift = shift
     @alph = alph
+    @alph_lc = alph.split('')
+    @alph_uc = alph.upcase.split('')
   end
 
   def cipher(string)
-    alph_lc = alph.split('')
-    alph_uc = alph.upcase.split('')
-    arr = string.split(//).map do |a|
-      if alph_lc.find_index(a) == nil
-        if alph_uc.find_index(a) == nil
+    array = string.split('').map do |a|
+      if @alph_lc.find_index(a) == nil
+        if @alph_uc.find_index(a) == nil
           a
         else
-          match = alph_uc.find_index(a)
-          alph_uc[(match.to_i + @shift)% 26 ]
+          match = @alph_uc.find_index(a)
+          @alph_uc[(match.to_i + @shift)% 26 ]
         end
       else
-        match = alph_lc.find_index(a)
-        alph_lc[(match.to_i + @shift)% 26 ]
+        match = @alph_lc.find_index(a)
+        @alph_lc[(match.to_i + @shift)% 26 ]
       end
     end
-       arr.join
+       array.join
   end
 end
 
-c = Caesar.new(5)
+c = Caesar.new(10)
 puts c.cipher("Hello, Hello, Hello world")
